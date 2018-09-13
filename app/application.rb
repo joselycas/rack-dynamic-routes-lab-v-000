d6b4f977f6d41cfe173b463009bba962c32df042
Jos Castro <joselycas@gmail.com>
2018-09-13 18:07:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,9 @@ class Application
     
     if req.path == /items/
       @@items.each do |item|
-        resp.write "#{item.price}\n"
         binding.pry
+        resp.write "#{item.price}\n"
+        
       end
         elsif req.path !~ (/items/)
          resp.write "Route not found"
