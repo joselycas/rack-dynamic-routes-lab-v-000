901c3acc460119458767bbab950143a360da3baa
Jos Castro <joselycas@gmail.com>
2018-09-13 17:29:39 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+  @@items = []
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+    end
+  end  
+  end
+end
